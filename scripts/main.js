0da5963168a115885722ef6ef2aa451cd86ccc83
Shalane Proctor <shalane088@gmail.com>
2022-06-02 16:04:52 -0500

    Added all the array method necissary for this build

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,10 @@ import { renderToDom } from "../utils/renderToDom.js";
 // Reusable function to get the cards on the DOM
 // .forEach()
 const renderCards = (array) => {
-  let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+  let refStuff = '';
+  array.forEach((item) => {
+    refStuff += card(item);
+  })
   renderToDom("#cards", refStuff);
 }
 
@@ -14,7 +17,11 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   const [, id] = event.target.id.split('--');
+   const index = referenceList.findIndex(num => num.id === Number(id))
+   referenceList[index].inCart = !referenceList[index].inCart;
+   cartTotal();
+   renderCards(referenceList)
   }
 }
 
@@ -22,23 +29,31 @@ const toggleCart = (event) => {
 // .filter()
 const search = (event) => {
   const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
+  const searchResult = referenceList.filter(userInput => 
+    userInput.title.toLowerCase().includes(eventLC) ||
+    userInput.author.toLowerCase().includes(eventLC) ||
+    userInput.description.toLowerCase().includes(eventLC)
+  )
+  renderCards(searchResult)
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    console.log('FREE')
+    const free = referenceList.filter(item => item.price <= 0);
+    renderCards(free)
   }
   if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+    const cart = referenceList.filter((item) => item.inCart);
+    renderCards(cart);
   }
   if(event.target.id.includes('books')) {
-    console.log('books!')
+    const book = referenceList.filter((item) => item.type === "Book");
+    renderCards(book);
   }
-  if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+  if(event.target.id.includes('clearFilter')) {;
+    renderCards(referenceList);
   }
   if(event.target.id.includes('cartList')) {
     cartList();
@@ -55,7 +70,7 @@ const buttonFilter = (event) => {
     <tbody>
     `;
     
-    productList().forEach(item => {
+    productList().sort((a, b) => a.type.localeCompare(b.type)).forEach(item => {
       table += tableRow(item);
     });
 
@@ -69,14 +84,24 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const cart = referenceList.filter(cartTotal => cartTotal.inCart)
+  const total = cart.reduce((a, b) => a + b.price, 0)
+  const free = cart.some(item => item.price <= 0);
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
+  if (free) {
+    document.querySelector('#includes-free').innerHTML = 'INCLUDES FREE ITEMS'
+  } else {
+    document.querySelector('#includes-free').innerHTML = '';
+  }
 }
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
+  return referenceList.map(item => ({ 
+    title: item.title, 
+    price: item.price, 
+    type: item.type }))
 }
 
 
